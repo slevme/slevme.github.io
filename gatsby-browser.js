e5874f6ba9758e0aeafcd4f86fd759daa5d09544
“황보영” <hby2233@naver.com>
2022-08-22 22:37:37 +0900

    Updates

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
-deckDeckGoHighlightElement();
